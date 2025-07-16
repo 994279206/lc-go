7a6d43d86892872175d1661ac3fdce8d3ec4d7ca
suhualin <suhualin.ytgx@bytedance.com>
2025-07-16 18:56:46 +0800

    删除有序数组中的重复项

--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -10,3 +10,9 @@ func TestFunc001(t *testing.T) {
 	ans := func001(n)
 	assert.Equal(t, 25, ans, "期望值相同")
 }
+func TestFunc002(t *testing.T) {
+	a := []int{0, 1, 2, 2, 3, 3, 4}
+	index := func002(a)
+	assert.Equal(t, 5, index, "期望值相同")
+
+}
